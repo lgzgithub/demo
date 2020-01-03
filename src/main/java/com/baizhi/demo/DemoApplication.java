278fe3a1da12148dd649f2425967a6b4f8c25127
lgz <2291605515@qq.com>
2020-01-03 13:32:37 +0800

    v4.0

--- src/main/java/com/baizhi/demo/DemoApplication.java
+++ src/main/java/com/baizhi/demo/DemoApplication.java
@@ -8,8 +8,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("你好");
-        System.out.println("adada");
-        System.out.println("谢谢");
+
     }
 }
